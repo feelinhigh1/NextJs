5cfde57c2ab63428de31bb786cc79fd2d38539e5
feelinhigh1 <sushan.poudel.55555@gmail.com>
2025-07-14 09:28:38 +0545

    Routing Metadata

--- routing-demo/src/app/about/page.tsx
+++ routing-demo/src/app/about/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+export const metadata = {
+  title: "About",
+  description: "About page of the application",
+};
+
 const page = () => {
   return <div>Abot me</div>;
 };
--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { Metadata } from "next";
 
-export default async function ProductDetails({
-  params,
-}: {
+type Props = {
   params: Promise<{ productId: string }>;
-}) {
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const productId = (await params).productId;
+  const title = await new Promise((resolve) =>
+    setTimeout(() => resolve(`Product ${productId} Details`), 100)
+  );
+  return {
+    title: `${title}`,
+    description: `Details about product ${productId}`,
+  };
+};
+
+export default async function ProductDetails({ params }: Props) {
   const productId = (await params).productId;
 
   return <div>Details about product {productId}</div>;
