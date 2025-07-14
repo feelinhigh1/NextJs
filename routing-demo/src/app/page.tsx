78715f345b5eb4b44b301d538232162715d2f0d5
feelinhigh1 <sushan.poudel.55555@gmail.com>
2025-07-14 10:03:35 +0545

    Link Component

--- routing-demo/src/app/page.tsx
+++ routing-demo/src/app/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Link from "next/link";
 
 const page = () => {
-  return <div>Welcome Home!</div>;
+  return (
+    <>
+      <h1>Welcome to My Page</h1>
+      <Link href="/blog">Go to Blog</Link>
+      <br />
+      <Link href="/products">Go to Product Page</Link>
+    </>
+  );
 };
 
 export default page;
--- routing-demo/src/app/products/page.tsx
+++ routing-demo/src/app/products/page.tsx
@@ -1,13 +1,29 @@
 import React from "react";
+import Link from "next/link";
 
 const page = () => {
+  const productId = 100; // Example product ID, can be dynamic
   return (
-    <div>
-      <h1>Products</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
-    </div>
+    <>
+      <div>
+        <h1>Products</h1>
+        <h2>
+          <Link href="/products/1">Product 1</Link>
+        </h2>
+        <h2>
+          <Link href="/products/2">Product 2</Link>
+        </h2>
+        <h2>
+          <Link href="/products/3" replace>
+            Product 3
+          </Link>
+        </h2>
+        <h2>
+          <Link href={`/products/${productId}`}>Product {productId}</Link>
+        </h2>
+      </div>
+      <Link href="/">Go Back Home</Link>
+    </>
   );
 };
 
